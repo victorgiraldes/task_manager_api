14ef6205aae7287e99436bae92cc83f13a546f65
Victor Giraldes <victorpgiraldes@gmail.com>
2018-11-22 13:38:59 -0300

    Create Api Version

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+require 'api_version_contraint'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/"  do
-
+      namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+      end
     end
-  end
 end
