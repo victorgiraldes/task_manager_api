d1b29b9a0f6b5e772d66e2f8ea0f249f7e157161
Victor Giraldes <victorpgiraldes@gmail.com>
2018-11-25 00:50:56 -0300

    tests with rspec

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,9 +4,9 @@
     let!(:user) { create(:user)}
     let(:user_id) { user.id }
 
-    before { host! 'api.taskmanager.test'}
+    before { host! 'api.task-manager.test'}
 
-    describe 'GET users/:id' do
+    describe 'GET /users/:id' do
       before do
         headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
         get "/users/#{user_id}", params: {}, headers: headers
@@ -14,8 +14,8 @@
 
       context 'when the user exists' do
         it 'returns the user' do
-          user_response = JSON.parse(response.body)
-          expect(user_response['id']).to eq(user_id)
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response[:id]).to eq(user_id)
         end
 
         it 'return status code 200' do
@@ -31,4 +31,40 @@
         end
       end
     end
+
+    describe 'POST /users' do
+
+      before do
+        headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+        post '/users', params: { user: user_params }, headers: headers
+      end
+
+      context 'when the request params are valid' do
+        let(:user_params) { attributes_for(:user) }
+
+        it 'returns status code 201' do
+          expect(response).to have_http_status(201)
+        end
+
+        it 'returns json data for the created user' do
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response[:email]).to eq(user_params[:email])
+        end
+      end
+
+      context 'when the request params are not valid' do
+        let (:user_params) { attributes_for(:user, email: 'invalid_email@') }
+
+        it 'returns status code 422' do
+          expect(response).to have_http_status(422)
+        end
+
+        it 'returns the json data for the errors' do
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response).to have_key(:errors)
+        end
+      end
+
+    end
+
   end
