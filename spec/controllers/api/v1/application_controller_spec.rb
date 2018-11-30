d2905c2a99555c657e901c5611e0b5bef76d14c4
Victor Giraldes <victorpgiraldes@gmail.com>
2018-11-30 16:52:57 -0300

    Adjusting restrictions with Devise Authenticable

---
+++ spec/controllers/api/v1/application_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+  RSpec.describe Api::V1::ApplicationController, type: :controller do
+    describe 'includes the corrects concerns' do
+      it { expect(controller.class.ancestors).to include(Authenticable) }
+    end
+
+  end
