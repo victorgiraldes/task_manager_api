78b934879fc8be8984ba8edfe4f025b5d46286c2
Victor Giraldes <victorpgiraldes@gmail.com>
2018-12-12 15:25:03 -0300

    associations with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates_uniqueness_of :auth_token
   before_create :generate_authentication_token!
 
+  has_many :tasks, dependent: :destroy
 
 
   def info
